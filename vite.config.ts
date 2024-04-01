556e1874898fbe5662605bc9d42c83b2cb299b87
Barath <iambharathpadhu@gmail.com>
2024-04-01 21:47:50 +0530

    - fix - updated the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("chakra") || id.includes("framer-motion")) {
+            return "vendor-ui";
+          }
+        },
+      },
+    },
+  },
 });
