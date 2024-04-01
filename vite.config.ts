e0a7bcbf859beb378256490349537b41cae77b8d
Barath <iambharathpadhu@gmail.com>
2024-04-01 18:33:15 +0530

    - fix: fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          vendor: ["lodash"],
-        },
-      },
-    },
-  },
 });
